becf083873c24ab14a74a0a4500263092e5e7f5e
SwLee7 <wondersw7@gmail.com>
2023-05-06 15:08:04 +0900

    normal loginForm ver.1_not test json-server yet

--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -1,19 +1,84 @@
-import styled from 'styled-components';
-// import Main from '../home/Main';
-import { Link } from 'react-router-dom';
+/*eslint-disable */
 
-const LoginButton = styled.button`
-  height: 50px;
-  width: 50px;
-  border: 1px solid black;
-`;
+import { Link, useNavigate } from 'react-router-dom';
+import { LoginForm, Input, LoginButton } from '../style/LoginStyle';
+import axios from 'axios';
+import { useState } from 'react';
+
+const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const validateEmail = (email) => {
+    return emailRegex.test(email);
+  };
+
+  const validatePassword = (password) => {
+    return passwordRegex.test(password);
+  };
+
+  const loginUser = (email, password) => {
+    axios
+      .get('http://localhost:5000/users', {
+        email,
+        password,
+      })
+      .then((response) => {
+        if (response.data.length > 0) {
+          console.log('로그인 잘 됨');
+        }
+      })
+      .catch((error) => {
+        console.log('보지 말아야할 것을 보고야 말았다.');
+        setError('Something going wrong...');
+      });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setError('The email is not a valid email address.');
+      return null;
+    }
+
+    if (!validatePassword(password)) {
+      setError('Password must be at least 8 characters');
+      return null;
+    }
+
+    await loginUser(email, password);
+  };
+
   return (
     <>
-      <LoginButton>
-        <Link to="/boards">Login</Link>
-      </LoginButton>
+      <LoginForm>
+        <div>
+          <img src="/Logo2.png" alt="식구 로고2" />
+        </div>
+        <Input
+          error={error}
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)} 
+          placeholder="Username" required />
+
+        <Input
+        error={error}
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)} 
+          placeholder="Password" required />
+          {error && <Error>{ error }</Error>}
+        <LoginButton type="submit" to="/boards">
+          Login
+        </LoginButton>
+      </LoginForm>
     </>
   );
 };
---
+++ client/src/style/LoginStyle.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+const LoginForm = styled.div`
+justify-content: center;
+align- items: center;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  margin: 10px 0;
+  padding: 12px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+`;
+
+const Button = styled.button`
+  width: 100%;
+  padding: 12px;
+  background-color: #ffc257;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f5b94a;
+  }
+`;
+
+const LoginButton = styled(Button)`
+  text-decoration: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    color: white;
+    text-decoration: #ffc250;
+  }
+`;
+export { LoginForm, Input, Button, LoginButton };

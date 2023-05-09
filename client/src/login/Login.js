5fa7b373f0282b70189962c3bbef7bb7b37e7a1f
SwLee7 <wondersw7@gmail.com>
2023-05-09 11:31:18 +0900

    login regex, password regex, error message

--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -14,14 +14,18 @@ import { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { login } from '../store/userSlice';
 
+const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
+  const [accessError, setAccessError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [fetchError, setFetchError] = useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  // const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
 
   const handleLogin = () => {
     axios
@@ -38,25 +42,43 @@ const Login = () => {
         if (user) {
           sessionStorage.setItem('user', JSON.stringify(user)); // 세션스토리지에 저장
           dispatch(login(user));
-          alert('로그인 성공!');
+          alert('로그인 되었습니다!');
           navigate('/boards');
         } else {
-          setError('이메일 또는 비밀번호가 잘못되었습니다.');
+          setAccessError('이메일 또는 비밀번호가 잘못되었습니다.');
         }
       })
       .catch((error) => {
-        setError('연결이 잘못되었습니다.');
+        setFetchError('연결이 잘못되었습니다.');
         console.error('로그인 에러:', error);
       });
   };
 
+  const validationEmail = (email) => {
+    return emailRegex.test(email);
+  };
+
+  const validationPassword = (password) => {
+    return passwordRegex.test(password);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    if (!validationEmail(email)) {
+      setEmailError('올바른 이메일 형식이 아닙니다.');
+      return null;
+    }
+    if (!validationPassword(password)) {
+      setPasswordError(
+        '비밀번호는 영문, 숫자를 포함하여 8자리 이상이어야 합니다.'
+      );
+      return null;
+    }
     handleLogin();
   };
 
   return (
-    <LoginForm onSubmit={handleSubmit}>
+    <LoginForm onSubmit={handleSubmit} noValidate>
       <h1>Login</h1>
       <Input
         type="email"
@@ -64,14 +86,17 @@ const Login = () => {
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
+      <Error>{validationEmail(email) ? null : emailError}</Error>
       <Input
         type="password"
         placeholder="Password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <Error>{error}</Error>
+      <Error>{validationPassword(password) ? null : passwordError}</Error>
+      <Error>{accessError}</Error>
       <LoginButton type="submit">Login</LoginButton>
+      <Error>{fetchError}</Error>
       <ForgotPasswordButton
         type="button"
         onClick={() => {

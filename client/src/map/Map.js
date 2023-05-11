0027694bd1e834bd25b3887eeba0c67fb84bd1e8
Gorden-Dosi <gosangmin1@gmail.com>
2023-05-11 17:28:18 +0900

    feat : naver지도에서 맛집불러오기 기능의 부재로 kakaomap으로 변경

--- client/src/map/Map.js
+++ client/src/map/Map.js
@@ -6,63 +6,23 @@ import { Mapbox } from './MapStyle';
 
 const Map = () => {
   const mapRef = useRef(null);
-  const mapInstance = useRef(null);
-
-  function onSuccessGeolocation(position, map, infowindow) {
-    const location = new window.naver.maps.LatLng(
-      position.coords.latitude,
-      position.coords.longitude
-    );
-
-    map.setCenter(location);
-    map.setZoom(15);
-
-    infowindow.setContent(
-      '<div style="padding:20px;">geolocation.getCurrentPosition() 위치</div>'
-    );
-
-    infowindow.open(map, location);
-    console.log('Coordinates: ' + location.toString());
-  }
-
-  function onErrorGeolocation(map, infowindow) {
-    const center = map.getCenter();
-
-    infowindow.setContent(
-      '<div style="padding:20px;"><h5 style="margin-bottom:5px;color:#f00;">Geolocation failed!</h5>latitude: ' +
-        center.lat() +
-        '<br />longitude: ' +
-        center.lng() +
-        '</div>'
-    );
-
-    infowindow.open(map, center);
-  }
 
   useEffect(() => {
-    if (window.naver && mapRef.current && !mapInstance.current) {
-      const mapOptions = {
-        center: new window.naver.maps.LatLng(37.3595704, 127.105399),
-        zoom: 7,
+    if (window.kakao && mapRef.current) {
+      const mapOption = {
+        center: new window.kakao.maps.LatLng(37.56779, 126.98051),
+        level: 4,
+        mapTypeId: window.kakao.maps.MapTypeId.ROADMAP,
       };
-      const map = new window.naver.maps.Map(mapRef.current, mapOptions);
-      mapInstance.current = map;
-    }
 
-    if (mapInstance.current) {
-      const infowindow = new window.naver.maps.InfoWindow();
+      const map = new window.kakao.maps.Map(mapRef.current, mapOption);
 
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(
-          (position) =>
-            onSuccessGeolocation(position, mapInstance.current, infowindow),
-          () => onErrorGeolocation(mapInstance.current, infowindow)
-        );
-      } else {
-        onErrorGeolocation(mapInstance.current, infowindow);
-      }
+      new window.kakao.maps.Marker({
+        position: new window.kakao.maps.LatLng(37.56779, 126.98051),
+        map: map,
+      });
     }
-  }, [mapRef.current]);
+  }, []);
 
   return (
     <MainWrap>

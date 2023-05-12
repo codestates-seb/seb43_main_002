43719d74b782da90a013b916f9e29f8de1dd774a
Gorden-Dosi <gosangmin1@gmail.com>
2023-05-12 18:08:20 +0900

    feat : 카카오맵  식당 정보 취득, index.html - 카카오 기능으로 정보변경

--- client/src/map/Map.js
+++ client/src/map/Map.js
@@ -6,9 +6,29 @@ import { Mapbox } from './MapStyle';
 
 const Map = () => {
   const mapRef = useRef(null);
+  const mapInstance = useRef(null);
+
+  // 위치를 표시하는 함수
+  const displayMarker = (locPosition, message) => {
+    var marker = new window.kakao.maps.Marker({
+      map: mapInstance.current,
+      position: locPosition,
+    });
+
+    var iwContent = message,
+      iwRemoveable = true;
+
+    var infowindow = new window.kakao.maps.InfoWindow({
+      content: iwContent,
+      removable: iwRemoveable,
+    });
+
+    infowindow.open(mapInstance.current, marker);
+    mapInstance.current.setCenter(locPosition);
+  };
 
   useEffect(() => {
-    if (window.kakao && mapRef.current) {
+    if (window.kakao && mapRef.current && !mapInstance.current) {
       const mapOption = {
         center: new window.kakao.maps.LatLng(37.56779, 126.98051),
         level: 4,
@@ -16,11 +36,45 @@ const Map = () => {
       };
 
       const map = new window.kakao.maps.Map(mapRef.current, mapOption);
+      mapInstance.current = map;
+      const places = new window.kakao.maps.services.Places();
 
-      new window.kakao.maps.Marker({
-        position: new window.kakao.maps.LatLng(37.56779, 126.98051),
-        map: map,
-      });
+      // Geolocation을 이용해서 접속 위치를 얻어옵니다
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          let lat = position.coords.latitude,
+            lon = position.coords.longitude;
+          let locPosition = new window.kakao.maps.LatLng(lat, lon),
+            message = '<div style="padding:5px;">현재위치</div>';
+          displayMarker(locPosition, message);
+          mapInstance.current.setCenter(locPosition); // 현재 위치로 지도의 중심을 업데이트합니다.
+        });
+      } else {
+        let locPosition = new window.kakao.maps.LatLng(37.56779, 126.98051),
+          message = '현재 위치 사용 불가';
+
+        displayMarker(locPosition, message);
+      }
+
+      // 카테고리 별로 검색하고 결과를 지도에 표시하는 함수
+      const searchAndDisplayPlacesByCategory = (category) => {
+        const callback = function (result, status) {
+          if (status === window.kakao.maps.services.Status.OK) {
+            result.forEach((place) => {
+              let locPosition = new window.kakao.maps.LatLng(place.y, place.x),
+                message = `<div style="padding:5px;">${category}: ${place.place_name}</div>`;
+              displayMarker(locPosition, message);
+            });
+          }
+        };
+
+        // 키워드로 장소 검색
+        places.keywordSearch(category, callback);
+      };
+
+      // 각 카테고리별로 장소를 검색하고 결과를 지도에 표시합니다
+      const categories = ['한식', '일식', '중식', '양식', '패스트푸드'];
+      categories.forEach(searchAndDisplayPlacesByCategory);
     }
   }, []);
 

a80cbb4734ebfbc4f4e2d484429bd59b3c1b60bb
dydwns9436 <dydwns9436@gmail.com>
2023-05-17 18:07:46 +0900

    통합22

---
+++ Server/src/main/generated/com/branch/sikgu/board/mapper/BoardMapperImpl.java
@@ -0,0 +1,13 @@
+package com.branch.sikgu.board.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-05-15T18:28:28+0900",
+    comments = "version: 1.5.1.Final, compiler: javac, environment: Java 11.0.18 (Azul Systems, Inc.)"
+)
+@Component
+public class BoardMapperImpl implements BoardMapper {
+}

85bace94733d3b1c603b0ee4bfe0a6ca2853ff29
SwLee7 <wondersw7@gmail.com>
2023-05-23 15:15:15 +0900

    restore Mypage_Header.js

--- client/src/mypage/Header.js
+++ client/src/mypage/Header.js
@@ -1,105 +1,43 @@
-// import { logout } from '../store/userSlice';
-// import { useNavigate } from 'react-router-dom';
-
-// import { useState } from 'react';
-// import { Title } from '../style/NewHeaderFooterStyle';
-// import { SearchSpan } from '../style/HomeStyle';
-// import { useDispatch } from 'react-redux';
-// import { setSearchTerm } from '../store/boardSlice';
-// // eslint-disable-next-line react/prop-types
-// const Header = ({ iconSrc, fnc, scrollPosition, scrollNumber }) => {
-//   const navigate = useNavigate();
-
-//   // props로 받아와서 네비게이터도 구현해야 한다.
-//   const [onSearch, setOnSerach] = useState(true);
-//   const [searchValue, setSearchValue] = useState('');
-
-//   const dispatch = useDispatch();
-
-//   function handleClick() {
-//     if (fnc === 'logout') {
-//       sessionStorage.removeItem('user');
-//       sessionStorage.removeItem('jwt');
-//       alert('로그아웃 되었습니다.');
-//       dispatch(logout());
-//       navigate('/');
-//     } else if (fnc === 'back') {
-//       navigate(-1);
-//     } else if (fnc === 'search') {
-//       if (onSearch) {
-//         setOnSerach(false);
-//       } else {
-//         setOnSerach(true);
-//       }
-//     }
-//   }
-//   const handleSearch = (e) => {
-//     setSearchValue(e.target.value);
-//     dispatch(setSearchTerm(e.target.value));
-//   };
-
-//   return (
-//     <Title>
-//       <div></div>
-//       {onSearch ? (
-//         <>
-//           <div>Sik:Gu</div>
-//           <button onClick={handleClick}>
-//             <img src={iconSrc} alt="아이콘" />
-//           </button>
-//         </>
-//       ) : (
-//         <>
-//           <SearchSpan
-//             value={searchValue}
-//             placeholder="Search..."
-//             onChange={handleSearch}
-//           ></SearchSpan>
-//           <button onClick={handleClick}>
-//             <img src={iconSrc} alt="아이콘" />
-//           </button>
-//         </>
-//       )}
-//     </Title>
-//   );
-// };
-
-// export default Header;
+import { logout } from '../store/userSlice';
 import { useNavigate } from 'react-router-dom';
+
 import { useState } from 'react';
 import { Title } from '../style/NewHeaderFooterStyle';
 import { SearchSpan } from '../style/HomeStyle';
 import { useDispatch } from 'react-redux';
 import { setSearchTerm } from '../store/boardSlice';
 // eslint-disable-next-line react/prop-types
-const Header = ({ iconSrc, fnc }) => {
+const Header = ({ iconSrc, fnc, scrollPosition, scrollNumber }) => {
   const navigate = useNavigate();
 
+  // props로 받아와서 네비게이터도 구현해야 한다.
   const [onSearch, setOnSerach] = useState(true);
   const [searchValue, setSearchValue] = useState('');
+
   const dispatch = useDispatch();
 
   function handleClick() {
     if (fnc === 'logout') {
-      console.log('로그아웃 됨');
+      sessionStorage.removeItem('user');
+      sessionStorage.removeItem('jwt');
+      alert('로그아웃 되었습니다.');
+      dispatch(logout());
+      navigate('/');
     } else if (fnc === 'back') {
       navigate(-1);
     } else if (fnc === 'search') {
-      // console.log('검색');
-      setOnSerach(false);
+      if (onSearch) {
+        setOnSerach(false);
+      } else {
+        setOnSerach(true);
+      }
     }
   }
-
-  const SearchPost = () => {
-    dispatch(setSearchTerm(searchValue));
-    setSearchValue('');
-    setOnSerach(true); // 검색 후에는 onSearch 값을 false로 변경
-  };
-
   const handleSearch = (e) => {
     setSearchValue(e.target.value);
+    dispatch(setSearchTerm(e.target.value));
   };
-  console.log(onSearch);
+
   return (
     <Title>
       <div></div>
@@ -117,7 +55,7 @@ const Header = ({ iconSrc, fnc }) => {
             placeholder="Search..."
             onChange={handleSearch}
           ></SearchSpan>
-          <button onClick={SearchPost}>
+          <button onClick={handleClick}>
             <img src={iconSrc} alt="아이콘" />
           </button>
         </>

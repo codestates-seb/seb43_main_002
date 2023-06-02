10b15be7478836718f153f33a2b3edb58fa2ccca
용장 <dyswkal5@gmail.com>
2023-06-02 09:39:09 +0900

    팔로워 리스트 기능 구현

---
+++ Server/src/main/java/com/branch/sikgu/myPage/dto/MyPageFollowerDto.java
@@ -0,0 +1,11 @@
+package com.branch.sikgu.myPage.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MyPageFollowerDto {
+    private Long myPageId;
+    private String nickName;
+}
